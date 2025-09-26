e14a00c793bcc407b689be4f47d802079046a900
tejas <tejasgaware9403@gmail.com>
2025-09-26 13:27:38 +0530

    feat(middleware): add reusable validation error handler

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,9 @@
 import errorHandlerMiddleware from "./errorHandlerMiddleware.js";
 import routeNotFoundMiddleware from "./routeNotFoundMiddleware.js";
+import handleValidationErrors from "./handleValidationErrors.middleware.js";
 
-export { errorHandlerMiddleware, routeNotFoundMiddleware };
+export {
+	errorHandlerMiddleware,
+	routeNotFoundMiddleware,
+	handleValidationErrors,
+};

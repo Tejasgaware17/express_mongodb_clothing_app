25944b879dc1d0b0195fc6dc0ad7f70463db80ae
tejas <tejasgaware9403@gmail.com>
2025-10-05 15:19:44 +0530

    feat(products): add validator for product creation

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,3 +1,4 @@
 export * from "./auth.validator.js";
 export * from "./user.validator.js";
 export * from "./category.validator.js";
+export * from "./product.validator.js";
---
+++ src/validators/product.validator.js
@@ -0,0 +1,58 @@
+import { body } from "express-validator";
+import { isValidObjectId } from "mongoose";
+
+export const createProductValidator = [
+	// Validation for base product
+	body("productType")
+		.notEmpty()
+		.withMessage("Product type is required.")
+		.isIn(["top-wear", "bottom-wear"])
+		.withMessage("Invalid product type."),
+
+	body("price")
+		.notEmpty()
+		.withMessage("Price is required.")
+		.isNumeric()
+		.withMessage("Price must be a number."),
+
+	body("description").notEmpty().withMessage("Description is required.").trim(),
+
+	body("gender")
+		.notEmpty()
+		.withMessage("Gender is required.")
+		.isIn(["men", "women", "unisex"])
+		.withMessage("Invalid gender."),
+
+	body("category")
+		.notEmpty()
+		.withMessage("Category is required.")
+		.custom((value) => {
+			if (!isValidObjectId(value)) {
+				throw new Error("Invalid category ID.");
+			}
+			return true;
+		}),
+
+	// Validation for product styles
+	body("style").isObject().withMessage("Style information is required."),
+	body("style.fit").notEmpty().withMessage("Fit is required for style."),
+	body("style.material")
+		.notEmpty()
+		.withMessage("Material is required for style."),
+	body("style.pattern")
+		.notEmpty()
+		.withMessage("Pattern is required for style."),
+
+	// Validation for product variants
+	body("variants")
+		.isArray({ min: 1 })
+		.withMessage("At least one product variant is required."),
+	body("variants.*.color").notEmpty().withMessage("Variant color is required."),
+	body("variants.*.sizes")
+		.isArray({ min: 1 })
+		.withMessage("At least one size is required for each variant."),
+	body("variants.*.sizes.*.size").notEmpty().withMessage("Size is required."),
+	body("variants.*.sizes.*.stock")
+		.isNumeric()
+		.withMessage("Stock must be a number."),
+];

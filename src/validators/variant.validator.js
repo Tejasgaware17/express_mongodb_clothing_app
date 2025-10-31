b06238475d4c99c42cd50b9dfcc50c640be14f44
tejas <tejasgaware9403@gmail.com>
2025-10-31 10:44:14 +0530

    feat(products): add validator for creating product variants

---
+++ src/validators/variant.validator.js
@@ -0,0 +1,19 @@
+import { body } from "express-validator";
+
+export const addVariantValidator = [
+	body("color").notEmpty().withMessage("Variant color is required.").trim(),
+
+	body("sizes")
+		.isArray({ min: 1 })
+		.withMessage("At least one size is required."),
+
+	body("sizes.*.size")
+		.notEmpty()
+		.withMessage("Size is required for each variant."),
+
+	body("sizes.*.stock")
+		.notEmpty()
+		.withMessage("Stock is required.")
+		.isNumeric({ no_symbols: true })
+		.withMessage("Stock must be a positive number."),
+];
